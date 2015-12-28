fb9804999ef2a7fc4cee77355748b21babccedd2
shibatanaoto <naoto.shibata510@gmail.com>
2015-12-28 18:32:43 +0900

    migration file

--- system/models.py
+++ system/models.py
@@ -2,4 +2,42 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Domain(models.Model):
+    """docstring for Domain"""
+    domain_name = models.CharField(max_length=255)
+    japanese = models.CharField(max_length=255)
+    updated_date = models.DateField()
+    domain_company = models.CharField(max_length=255)
+    representative = models.CharField(max_length=255)
+
+
+class Site(models.Model):
+    """docstring for Site"""
+    group = models.CharField(max_length=255)
+    site_title = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    japanese = models.CharField(max_length=255)
+    server = models.CharField(max_length=255)
+
+
+class Server(models.Model):
+    """docstring for Server"""
+    server_company = models.CharField(max_length=255)
+    updated_date = models.DateField()
+    host = models.CharField(max_length=255)
+
+
+class SiteComment(models.Model):
+    """docstring for SiteComment"""
+    site_id = models.IntegerField()
+    comment = models.CharField(max_length=255)
+    created_at = models.DateField()
+
+
+class Link(models.Model):
+    """docstring for Link"""
+    from_site = models.IntegerField()
+    to_site = models.IntegerField()
+    link_position = models.CharField(max_length=255)
+    created_at = models.DateField()
--- system/views.py
+++ system/views.py
@@ -33,3 +33,58 @@ def server_unup(request):
 @login_required
 def domain_unup(request):
     return render(request, 'system/domain_unup.html')
+
+
+@login_required
+def domain_detail(request):
+    return render(request, 'system/domain_detail.html')
+
+
+@login_required
+def server_detail(request):
+    return render(request, 'system/server_detail.html')
+
+
+@login_required
+def update_domain(request):
+    return render(request, 'system/update_domain.html')
+
+
+@login_required
+def update_server(request):
+    return render(request, 'system/update_server.html')
+
+
+@login_required
+def site_detail(request):
+    return render(request, 'system/site_detail.html')
+
+
+@login_required
+def link(request):
+    return render(request, 'system/link.html')
+
+
+@login_required
+def create_server(request):
+    return render(request, 'system/create_server.html')
+
+
+@login_required
+def create_domain(request):
+    return render(request, 'system/create_domain.html')
+
+
+@login_required
+def create_site(request):
+    return render(request, 'system/create_site.html')
+
+
+@login_required
+def create_link(request):
+    return render(request, 'system/create_link.html')
+
+
+@login_required
+def setting(request):
+    return render(request, 'system/setting.html')
