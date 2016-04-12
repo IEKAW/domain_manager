99c3eded02088ea5426f261ec0df28811b7bf57d
shibatanaoto <naoto.shibata510@gmail.com>
2016-04-12 14:56:25 +0900

    A!

--- system/views.py
+++ system/views.py
@@ -242,10 +242,13 @@ def domain(request):
                 tmp['japanese'] = domain_data[2]
                 tmp['updated_at'] = domain_data[3]
                 tmp['company'] = domain_data[4]
-                setting_domain = Setting_Domain.objects.get(
-                    domain_company=domain_data[4]
-                )
-                tmp['company_url'] = setting_domain.login_url
+                try:
+                    setting_domain = Setting_Domain.objects.get(
+                        domain_company=domain_data[4]
+                    )
+                    tmp['company_url'] = setting_domain.login_url
+                except:
+                    tmp['company_url'] = ""
                 tmp['representative'] = "not selected"
                 if tmp['japanese'][:7] == 'http://':
                     tmp['japanese'] = tmp['japanese'][7:]
